7028e56cc7bbed3ed514dd41a27110a11160817b
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2024-06-19 18:49:59 -0400

    Fix div not appearing on some websites

--- carciofo-web-blocker/content-script.js
+++ carciofo-web-blocker/content-script.js
@@ -1,13 +1,18 @@
-let screenBlocker;
+(() => {
+    let screenBlocker;
 
-screenBlocker = document.createElement("div");
-screenBlocker.setAttribute("id", "carciofo-blocker");
-screenBlocker.style.backgroundColor = "white";
-screenBlocker.style.position = "fixed";
-screenBlocker.style.height = "100vh";
-screenBlocker.style.width = "100vw";
-screenBlocker.style.zIndex = 99999;
+    screenBlocker = document.createElement("div");
+    screenBlocker.setAttribute("id", "carciofo-blocker");
+    screenBlocker.style.backgroundColor = "white";
+    screenBlocker.style.position = "fixed";
+    screenBlocker.style.height = "100vh";
+    screenBlocker.style.width = "100vw";
+    screenBlocker.style.zIndex = 99999;
+    screenBlocker.style.top = "0px";
+    screenBlocker.style.left = "0px";
 
-let body = document.querySelector("body");
-body.classList.add("no-scroll");
-body.appendChild(screenBlocker);
+    let body = document.querySelector("body");
+    body.classList.add("no-scroll");
+    body.style.overflow = "hidden";
+    body.appendChild(screenBlocker);
+})();
