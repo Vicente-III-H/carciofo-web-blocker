ed4be76463b2332966519f781c58f2899b2eb31e
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2024-07-02 19:42:27 -0400

    Add load saves functionality in options

--- carciofo-web-blocker/options/options.js
+++ carciofo-web-blocker/options/options.js
@@ -8,18 +8,47 @@ function getLinksFromTextArea(textarea) {
     return links;
 };
 
+function setTextAreaLinks(listArray) {
+    let text = "";
+
+    for (i = 0; i < (listArray.length - 1); i++) {
+        text += listArray[i] + "\n";
+    }
+    text += listArray[listArray.length - 1];
+
+    return text;
+}
+
 const saveOptions = () => {
     const blacklist = getLinksFromTextArea(document.querySelector("#blacklist"));
     const strictBlacklist = getLinksFromTextArea(document.querySelector("#strict-blacklist"));
 
     chrome.storage.sync.set({
-        [blacklist]: JSON.stringify(blacklist),
-        [strictBlacklist]: JSON.stringify(strictBlacklist)
-    },
-    () => {
+        "blacklist": JSON.stringify(blacklist),
+        "strictBlacklist": JSON.stringify(strictBlacklist)
+    }, () => {
         const saveResponse = document.querySelector("#save-response");
         saveResponse.innerText = "Changes saved"
     });
 };
 
+const retrieveOptions = () => {
+    const listsPromise = chrome.storage.sync.get(["blacklist", "strictBlacklist"]);
+
+    listsPromise.then((listsObject) => {
+        const blacklist = listsObject.blacklist;
+        const strictBlacklist = listsObject.strictBlacklist;
+
+        if (typeof(blacklist) != typeof(undefined)) {
+            document.querySelector("#blacklist").value = setTextAreaLinks(JSON.parse(blacklist));
+            console.log("blacklist loaded");
+        };
+        if (typeof(strictBlacklist) != typeof(undefined)) {
+            document.querySelector("#strict-blacklist").value = setTextAreaLinks(JSON.parse(strictBlacklist));
+            console.log("strict blacklist loaded");
+        };
+    })
+}
+
+document.addEventListener("DOMContentLoaded", retrieveOptions);
 document.querySelector("#save-button").addEventListener("click", saveOptions);
