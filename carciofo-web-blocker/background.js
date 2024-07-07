a03af5f79bb3c883956ed22aad747c8e710f0816
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2024-07-07 17:33:11 -0400

    Implement storage access to service worker

--- carciofo-web-blocker/background.js
+++ carciofo-web-blocker/background.js
@@ -1,28 +1,52 @@
-const blacklist = ["youtube.com/shorts"];
-const strictBlacklist = ["https://www.youtube.com/"];
+function linkInLists(link, blacklist, strictBlacklist) {
+    return strictBlacklist.includes(link) || Boolean(blacklist.find((linkFromList) => link.includes(linkFromList)));
+}
 
-function matchNameInList(name, matchList) {
-    for (let i = 0; i < matchList.length; i++) {
-        if (name.includes(matchList[i])) {
-            return true;
-        };
-    };
-    return false;
-};
+(async () => {
+    let blacklist = [];
+    let strictBlacklist = [];
 
-chrome.tabs.onUpdated.addListener((tabId, tabs) => {
-    if (tabs.url) {
-        const tabHostname = new URL(tabs.url);
-        console.log(tabHostname.href);
-        
-        console.log(strictBlacklist.includes(tabHostname.href))
-        if (matchNameInList(tabHostname.href, blacklist) || strictBlacklist.includes(tabHostname.href)) {
-            console.log("blacklisted!!");
-            
-            chrome.scripting.executeScript({
-                target: {tabId: tabId},
-                files: ["content-script.js"]
-            });
+    try {
+        const listObject = await chrome.storage.sync.get(["blacklist", "strictBlacklist"]);
+
+        if (typeof(listObject.blacklist) !== typeof(undefined)) {
+            blacklist = JSON.parse(listObject.blacklist);
         }
+        if (typeof(listObject.strictBlacklist) !== typeof(undefined)) {
+            strictBlacklist = JSON.parse(listObject.strictBlacklist);
+        }
+
+    } finally {
+        console.log(blacklist);
+        console.log(strictBlacklist);
+
+        chrome.storage.sync.onChanged.addListener((changesObject) => {
+            if (changesObject.blacklist) {
+                blacklist = JSON.parse(changesObject.blacklist.newValue);
+            }
+            if (changesObject.strictBlacklist) {
+                strictBlacklist = JSON.parse(changesObject.strictBlacklist.newValue);
+            }
+
+            console.log(blacklist);
+            console.log(strictBlacklist);
+        });
+
+        chrome.tabs.onUpdated.addListener((tabId, tabs) => {
+            if (tabs.url) {
+                const tabHostname = new URL(tabs.url);
+                console.log(tabHostname.href);
+                
+                if (linkInLists(tabHostname.href, blacklist, strictBlacklist)) {
+                    console.log(linkInLists(tabHostname.href, blacklist, strictBlacklist));
+                    console.log("blacklisted!!");
+                    
+                    chrome.scripting.executeScript({
+                        target: {tabId: tabId},
+                        files: ["content-script.js"]
+                    });
+                }
+            }
+        })
     }
-})
+})();
