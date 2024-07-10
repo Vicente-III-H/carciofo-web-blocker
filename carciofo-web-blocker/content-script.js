04e61ca8520456304b37fff82debeb010e998bed
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2024-07-09 21:08:43 -0400

    Add styling to web blocker

--- carciofo-web-blocker/content-script.js
+++ carciofo-web-blocker/content-script.js
@@ -1,7 +1,5 @@
-(() => {
-    let screenBlocker;
-
-    screenBlocker = document.createElement("div");
+function createBlocker() {
+    let screenBlocker = document.createElement("div");
     screenBlocker.setAttribute("id", "carciofo-blocker");
     screenBlocker.style.backgroundColor = "white";
     screenBlocker.style.position = "fixed";
@@ -11,8 +9,43 @@
     screenBlocker.style.top = "0px";
     screenBlocker.style.left = "0px";
 
+    screenBlocker.style.display = "flex";
+    screenBlocker.style.flexDirection = "column";
+    screenBlocker.style.justifyContent = "center";
+    screenBlocker.style.alignItems = "center";
+
+    let icon = document.createElement("img");
+    icon.setAttribute("src", chrome.runtime.getURL("icon.PNG"));
+    icon.setAttribute("width", "250px");
+    icon.setAttribute("height", "250px");
+    icon.style.width = "250px";
+    icon.style.height = "250px";
+    //icon.setAttribute("alt", "");
+    icon.style.marginBottom = "24px";
+
+    let text = document.createElement("div");
+    text.innerHTML = "Make like a tree and leaf!";
+    text.style.fontFamily = "Arial, sans-serif";
+    text.style.fontWeight = "900";
+    text.style.fontSize = "28px";
+    text.style.color = "rgb(88, 171, 61)";
+
+    screenBlocker.appendChild(icon);
+    screenBlocker.appendChild(text);
+
+    return screenBlocker;
+}
+
+(() => {
     let body = document.querySelector("body");
-    body.classList.add("no-scroll");
-    body.style.overflow = "hidden";
-    body.appendChild(screenBlocker);
+
+    if (!body.querySelector("#carciofo-blocker")) {
+        let screenBlocker = createBlocker();
+
+        body.classList.add("no-scroll");
+        body.style.overflow = "hidden";
+
+        body.appendChild(screenBlocker);
+    }
+
 })();
