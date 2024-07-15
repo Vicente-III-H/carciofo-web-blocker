1b352219a650fa4fa96a61264afa329aaec8cf5c
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2024-07-15 14:13:05 -0400

    Make blocker styles more readable

--- carciofo-web-blocker/content-script.js
+++ carciofo-web-blocker/content-script.js
@@ -1,39 +1,44 @@
 function createBlocker() {
     let screenBlocker = document.createElement("div");
     screenBlocker.setAttribute("id", "carciofo-blocker");
-    screenBlocker.style.backgroundImage = "url(" + chrome.runtime.getURL("images/options-background.jpg") + ")";
-    screenBlocker.style.backgroundColor = "rgb(14 26 15)";
-    screenBlocker.style.backgroundRepeat = "no-repeat";
-    screenBlocker.style.backgroundPosition = "center";
-    screenBlocker.style.backgroundSize = "cover";
-    screenBlocker.style.position = "fixed";
-    screenBlocker.style.height = "100vh";
-    screenBlocker.style.width = "100vw";
-    screenBlocker.style.zIndex = 99999;
-    screenBlocker.style.top = "0px";
-    screenBlocker.style.left = "0px";
+    screenBlocker.style.cssText = `
+        background-image: url(${chrome.runtime.getURL("images/options-background.jpg")});
+        background-color: rgb(14 26 15);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        position: fixed;
+        height: 100vh;
+        width: 100vw;
 
-    screenBlocker.style.display = "flex";
-    screenBlocker.style.flexDirection = "column";
-    screenBlocker.style.justifyContent = "center";
-    screenBlocker.style.alignItems = "center";
+        z-index: 99999;
+        top: 0px;
+        left: 0px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    `;
 
     let background = document.createElement("div");
-    background.style.width = "468px";
-    background.style.height = "400px";
-    background.style.backgroundColor = "rgba(66, 87, 60, 0.75)";
-    background.style.backdropFilter = "blur(6px)";
-    background.style.borderRadius = "40px";
-    background.style.boxShadow = "0px 5px 10px 10px rgba(0, 0, 0, 0.25)";
-    background.style.color = "white";
-    background.style.fontFamily = "system-ui, sans-serif";
-    background.style.overflow = "hidden";
+    background.style.cssText = `
+        width: 468px;
+        height: 400px;
+        background-color: rgba(66, 87, 60, 0.75);
+        backdrop-filter: blur(6px);
+        border-radius: 40px;
+        box-shadow: 0px 5px 10px 10px rgba(0, 0, 0, 0.25);
+        color: white;
+        font-family: system-ui, sans-serif;
+        overflow: hidden;
 
-    background.style.display = "flex";
-    background.style.flexDirection = "column";
-    background.style.justifyContent = "center";
-    background.style.alignItems = "center";
-    background.style.gap = "24px";
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 24px;
+    `;
 
     let icon = document.createElement("img");
     icon.setAttribute("src", chrome.runtime.getURL("images/icon-outline.PNG"));
@@ -44,7 +49,7 @@ function createBlocker() {
     //icon.setAttribute("alt", "");
 
     let text = document.createElement("div");
-    text.innerHTML = "Website has been blocked.";
+    text.innerHTML = "Website has been blocked";
     text.style.fontWeight = "600";
     text.style.fontSize = "32px";
 
